8b8616eccd4173672b1aebc1d193ebc09f14412f
rodrigo123451 <85976210+rodrigo123451@users.noreply.github.com>
2021-09-22 22:14:56 -0400

    Add files via upload

---
+++ pregunta4/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    
+
+
+    let listSuma=[for i in 1..5 ->4]
+    let suma=List.sum listSuma
+    printf "%A" suma
+    
+  
+    
+
+
+    Console.ReadKey() |>ignore
+    0 // return an integer exit code
+   
